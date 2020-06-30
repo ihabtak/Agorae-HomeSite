33709e5c996c9375307c718b112376c73374daf5
Ihab T <ihabtak@users.noreply.github.com>
2020-07-01 01:24:45 +0200

    🐛 react-mail removed

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import "./Contact.css"
 //import { ContactForm } from "./Form"
-import ReactContactForm from 'react-mail-form';
 
 export const Contact = () => (
         <div className="ctt">
             <h1>Contact form</h1>
-            <ReactContactForm to="ihabtak@gmail.com" />
+            <p>In progress ...</p>
         </div>
 )
