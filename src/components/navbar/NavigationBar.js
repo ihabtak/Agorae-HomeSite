2fa18a770580f92a60291e7d1ba045e6e42398a6
Ihab T <ihabtak@users.noreply.github.com>
2020-07-03 17:25:15 +0200

    💄 Fonts update

--- src/components/navbar/NavigationBar.js
+++ src/components/navbar/NavigationBar.js
@@ -1,77 +1,104 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
-import styled from 'styled-components';
-import { translate } from 'react-i18next';
-import InterL from './InterL';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import styled from "styled-components";
+import { translate } from "react-i18next";
+import InterL from "./InterL";
 
 const Styles = styled.div`
-    .navbar {
-        
-        font-family: Trebuchet MS;
-    }
+  #nbr {
+    -webkit-box-shadow: 2.5px 2.5px 2.5px rgba(0, 0, 0, 0.4);
+    -moz-box-shadow: 2.5px 2.5px 2.5px rgba(0, 0, 0, 0.4);
+    box-shadow: 2.5px 2.5px 2.5px 0px rgba(0, 0, 0, 0.4);
+  }
+  .navbar {
+    font-family: Trebuchet MS;
+  }
+  .nav-link.active {
+    color: paleturquoise !important;
+  }
 
-    .clrtxtcyan {
-        background-color: #144f5d;
-    }
+  .clrtxtcyan {
+    background-color: #144f5d;
+  }
 
-    .navbar-brand{
-       color:  white;
-
+  .navbar-brand {
+    color: white;
+  }
+  .navbar-nav .nav-link {
+    color: white;
+    &:hover {
+      color: #144f5d;
+      background-color: white;
     }
-     .navbar-nav .nav-link{
-        color:  white;
-        &:hover {
-            color: #144f5d;
-            background-color: white;
-        }
-     }
-    .logo {
-        font-size: 2rem;
+  }
+  .logo {
+    font-size: 2rem;
+  }
+  @media only screen and (min-width: 992px) {
+    .nav-link {
+      padding-right: 1rem !important;
+      padding-left: 1rem !important;
     }
-    @media only screen and (min-width: 992px) {
-        .nav-link{
-            padding-right: 1rem !important;
-            padding-left: 1rem !important;
-        }  
-    }
-    
+  }
 `;
 
-const NavigationBar = ({ t }) =>(
-    <Styles>
-        <Navbar collapseOnSelect className="clrtxtcyan" expand="lg" fixed="top" id="nbr" >
-            <Navbar.Brand href="/" className="logo">MAP21</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Item>
-                        <Nav.Link as={Link} to='/'>{t('navbar.accueil')}</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link as={Link} to='/projetmap21'>{t('navbar.projetmap21')}</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link as={Link} to='/nuagedethemes'>{t('navbar.nuagedethemes')}</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link as={Link} to='/nouveauitem'>{t('navbar.nouveauItem')}</Nav.Link> 
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link as={Link} to='/syllabus'>{t('navbar.syllabus')}</Nav.Link> 
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link as={Link} to='/guide'>{t('navbar.guide')}</Nav.Link>  
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link as={Link} to='/contact'>{t('navbar.contact')}</Nav.Link>  
-                    </Nav.Item>
-                    <Nav.Item>
-                        <InterL/> 
-                    </Nav.Item>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </Styles>
-)
-export default translate('common')(NavigationBar);
+const NavigationBar = ({ t }) => (
+  <Styles>
+    <Navbar
+      collapseOnSelect
+      className="clrtxtcyan"
+      expand="lg"
+      fixed="top"
+      id="nbr"
+    >
+      <Navbar.Brand href="/" className="logo">
+        MAP21
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link eventKey="1" as={Link} to="/">
+              {t("navbar.accueil")}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="2" as={Link} to="/projetmap21">
+              {t("navbar.projetmap21")}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="3" as={Link} to="/nuagedethemes">
+              {t("navbar.nuagedethemes")}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="4" as={Link} to="/nouveauitem">
+              {t("navbar.nouveauItem")}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="5" as={Link} to="/syllabus">
+              {t("navbar.syllabus")}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="6" as={Link} to="/guide">
+              {t("navbar.guide")}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="7" as={Link} to="/contact">
+              {t("navbar.contact")}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <InterL />
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+);
+export default translate("common")(NavigationBar);
